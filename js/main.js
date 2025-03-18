082e5a836b19be001b47850611ad2570d92ecb52
mehush <tolstys@gmail.com>
2025-03-18 19:26:09 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@
 
 $(function () {
 	let items = {
-		tanks: [[250, .03], [450, .05], [600, .10], [800, .06], [950, .07]],
+		tanks: [[250, .03], [500, .05], [650, .10], [900, .06], [1050, .07]],
 		fencers: [[160, .05], [310, .06], [440, .07], [600, .08], [810, .09]],
-		tricksters: [[100, .09], [160, .09], [200, .04], [380, .08], [520, .04]],
+		tricksters: [[100, .09], [160, .09], [200, .04], [380, .09], [500, .04]],
 		fighters: [[120, .06], [230, .07], [450, .09], [600, .09], [770, .03]],
 		healers: [[50, .10], [90, .10], [180, .10], [360, .10], [450, .10]],
-		throwers: [[80, .07], [110, .07], [150, .10], [200, .07], [250, .07]],
+		throwers: [[80, .07], [110, .07], [150, .10], [180, .07], [220, .07]],
 		shooters: [[0, .00], [40, .10], [70, .04], [130, .07], [180, .09]],
 		cultists: [[100, .05], [200, .06], [300, .07], [400, .08], [500, .09]],
-		mages: [[10, .07], [40, .10], [60, .04], [110, .08], [800, .06]],
+		mages: [[30, .07], [40, .10], [60, .04], [110, .08], [800, .06]],
 		eggheads: [[110, .06], [140, .06], [210, .10], [280, .10], [700, .10]],
 		newbies: [[70, 0], [100, 0], [150, 0], [200, 0], [250, 0]],
+		summons: [
+			[[1313, 2626, 3939], [300], [600], [2000]],
+			[[300, 500, 900], [4500], [7000], [10000]]
+		],
 	};
 	let img = new Image(684, 334);
 	img.onload = calc;
@@ -26,7 +30,7 @@ $(function () {
 			flat = parseInt($("input[name=flat]").val(), 10) || 0,
 			percent = parseInt($("input[name=percent]").val(), 10) || 0,
 			bonus = parseInt($("input[name=bonus]").val(), 10) || 0,
-			fat = $("input[name=fat]").is(":checked") ? 1.35 : 1,
+			fat = $("input[name=fat]").is(":checked") ? 1.25 : 1,
 			bear = $("input[name=bear]").is(":checked") ? 1.3 : 1,
 			fit = $("input[name=fit]").is(":checked") ? 1.1 : 1,
 			flat_e = parseInt($("input[name=flat_e]").val(), 10) || 0,
@@ -54,6 +58,19 @@ $(function () {
 					ctx.fillStyle = "rgba(76, 175, 80, 0.9)";
 					ctx.fillText(hp, +coords[0] + (47 - width) / 2, +coords[1] + 58);
 					mega += parseInt($("input[name='lvl_" + (j + 1) + "']").val(), 10) * hp;
+				});
+			} else if (itemClass === 'summons') {
+				$.each(itemsClass, function(j, items){
+					$.each(items, function(k, item){
+						let hp = (level <= item.length ? item[level - 1] : item[item.length - 1]) * (summons / 100 + 1) * fit;
+						hp = parseInt(hp, 10);
+						let coords = [160 + j * 31 + (i + j) * 56, 30 + k * 75, 123 + (i + j) * 56];
+						var width = ctx.measureText(hp).width;
+						ctx.fillStyle = "rgba(20, 15, 20, 0.8)";
+						ctx.fillRect(+coords[0] + (47 - width) / 2 - 6, +coords[1] + 58 - fontSize, width + 12, fontSize + 4);
+						ctx.fillStyle = "rgba(76, 175, 80, 0.9)";
+						ctx.fillText(hp, +coords[0] + (47 - width) / 2, +coords[1] + 58);
+					});
 				});
 			} else {
 				$.each(itemsClass, function(j, item){
